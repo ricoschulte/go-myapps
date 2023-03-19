a15834aa8f0823d6c4db267eeeaf17e319313ed6
Rico Schulte <rico.schulte@gmx.de>
2023-03-19 11:35:22 +0100

    PbxAdminApi/GetAppLics

--- service/pbxadminapi/api.go
+++ service/pbxadminapi/api.go
@@ -1,11 +1,20 @@
 package pbxadminapi
 
 import (
+	"encoding/json"
+	"sync"
+
 	"github.com/ricoschulte/go-myapps/service"
 	log "github.com/sirupsen/logrus"
 )
 
 type PbxAdminApi struct {
+	mu        sync.Mutex
+	receivers []chan PbxAdminApiEvent
+}
+
+func NewPbxAdminApi() *PbxAdminApi {
+	return &PbxAdminApi{}
 }
 
 func (api *PbxAdminApi) GetApiName() string {
@@ -13,13 +22,55 @@ func (api *PbxAdminApi) GetApiName() string {
 }
 
 func (api *PbxAdminApi) OnConnect(connection *service.AppServicePbxConnection) {
-	log.WithField("api", api.GetApiName()).Warn("OnConnect not implemented ")
+	api.sendEvent(PbxAdminApiEvent{Type: PbxAdminApiEventConnect, Connection: connection})
 }
 
 func (api *PbxAdminApi) OnDisconnect(connection *service.AppServicePbxConnection) {
-	log.WithField("api", api.GetApiName()).Warn("OnDisconnect not implemented ")
+	api.sendEvent(PbxAdminApiEvent{Type: PbxAdminApiEventDisconnect, Connection: connection})
 }
 
 func (api *PbxAdminApi) HandleMessage(connection *service.AppServicePbxConnection, msg *service.BaseMessage, message []byte) {
-	log.WithField("api", api.GetApiName()).Warn("HandleMessage not implemented ")
+	switch msg.Mt {
+	case "GetAppLicsResult":
+		msg := GetAppLicsResult{}
+		if err := json.Unmarshal(message, &msg); err != nil {
+			log.Errorf("PbxApi: error unmarshalling message: %v", err)
+		}
+		api.sendEvent(PbxAdminApiEvent{Type: PbxAdminApiGetAppLicsResult, GetAppLicsResult: &msg, Connection: connection})
+	default:
+		log.Warn("unknown message received: %s", msg.Mt)
+	}
+}
+
+func (api *PbxAdminApi) AddReceiver() chan PbxAdminApiEvent {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+	ch := make(chan PbxAdminApiEvent)
+	api.receivers = append(api.receivers, ch)
+	return ch
+}
+
+func (api *PbxAdminApi) RemoveReceiver(ch chan PbxAdminApiEvent) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	for i, c := range api.receivers {
+		if c == ch {
+			// Remove the channel from the slice
+			api.receivers = append(api.receivers[:i], api.receivers[i+1:]...)
+			// Close the channel to signal the receiver that it should stop listening
+			close(ch)
+			return
+		}
+	}
+}
+
+func (api *PbxAdminApi) sendEvent(event PbxAdminApiEvent) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	// Send the event to all registered receivers
+	for _, ch := range api.receivers {
+		ch <- event
+	}
 }
---
+++ service/pbxadminapi/structs.go
@@ -0,0 +1,83 @@
+package pbxadminapi
+
+import (
+	"strconv"
+
+	"github.com/ricoschulte/go-myapps/service"
+)
+
+// Request the available App Licenses
+type GetAppLics struct {
+	service.BaseMessage
+}
+
+func NewGetAppLics(src string) *GetAppLics {
+	return &GetAppLics{
+		BaseMessage: service.BaseMessage{
+			Api: "PbxAdminApi",
+			Mt:  "GetAppLics",
+			Src: src,
+		},
+	}
+}
+
+type GetAppLicsResult struct {
+	service.BaseMessage
+	Licenses []AppLic `json:"lic"`
+}
+
+type AppLic struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	Count   string `json:"count"`
+	Used    string `json:"used"`
+	Local   string `json:"local"`
+	Slaves  string `json:"slaves"`
+	Key     string `json:"key"`
+}
+
+// returns the value as float64 or errValue when the string from the JSON couldnt be parsed as float
+func (l *AppLic) GetCount(errValue float64) float64 {
+	if s, err := strconv.ParseFloat(l.Count, 32); err == nil {
+		return s
+	} else {
+		return errValue
+	}
+}
+
+// returns the value as float64 or errValue when the string from the JSON couldnt be parsed as float
+func (l *AppLic) GetUsed(errValue float64) float64 {
+	if s, err := strconv.ParseFloat(l.Used, 32); err == nil {
+		return s
+	} else {
+		return errValue
+	}
+}
+
+// returns the value as float64 or errValue when the string from the JSON couldnt be parsed as float
+func (l *AppLic) GetLocal(errValue float64) float64 {
+	if s, err := strconv.ParseFloat(l.Local, 32); err == nil {
+		return s
+	} else {
+		return errValue
+	}
+}
+
+// returns the value as float64 or errValue when the string from the JSON couldnt be parsed as float
+func (l *AppLic) GetSlaves(errValue float64) float64 {
+	if s, err := strconv.ParseFloat(l.Slaves, 32); err == nil {
+		return s
+	} else {
+		return errValue
+	}
+}
+
+type PbxAdminApiEvent struct {
+	Type             int
+	Connection       *service.AppServicePbxConnection
+	GetAppLicsResult *GetAppLicsResult
+}
+
+const PbxAdminApiEventDisconnect = -20
+const PbxAdminApiEventConnect = -10
+const PbxAdminApiGetAppLicsResult = 10
